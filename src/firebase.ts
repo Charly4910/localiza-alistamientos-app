591cb2d982f8ba424dde67024596e72c9cbd5f70
Charly4910 <166964297+Charly4910@users.noreply.github.com>
2025-07-06 16:54:29 -0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyAzcZgC5TpJEzz2xDpgYMJp3ycJj7izRcA",
   authDomain: "localiza-alistamientos-6a6b9.firebaseapp.com",
   projectId: "localiza-alistamientos-6a6b9",
-  storageBucket: "localiza-alistamientos-6a6b9.appspot.com", // 🔧 CORREGIDO
+  storageBucket: "localiza-alistamientos-6a6b9.appspot.com", // 🔧 importante que diga .app**spot**.com
   messagingSenderId: "504938867763",
   appId: "1:504938867763:web:7718feb7f5964583bfc746",
   measurementId: "G-3ZNXPES7S1"
 };
 
-// Inicializa la app
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 getAnalytics(app);
 
