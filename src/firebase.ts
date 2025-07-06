a0cf9244b0a6134919760676d6a1b5ecb64c8a75
Charly4910 <166964297+Charly4910@users.noreply.github.com>
2025-07-06 16:50:43 -0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,21 @@
-// src/lib/firebase.ts
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Tu configuración de Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAzcZgC5TpJEzz2xDpgYMJp3ycJj7izRcA",
+  authDomain: "localiza-alistamientos-6a6b9.firebaseapp.com",
+  projectId: "localiza-alistamientos-6a6b9",
+  storageBucket: "localiza-alistamientos-6a6b9.firebasestorage.app",
+  messagingSenderId: "504938867763",
+  appId: "1:504938867763:web:7718feb7f5964583bfc746",
+  measurementId: "G-3ZNXPES7S1"
 };
 
-// Inicializa Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
