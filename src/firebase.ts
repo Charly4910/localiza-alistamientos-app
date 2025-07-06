1db3b6e676d46affbcdf03d707679846ef5852fe
Charly4910 <166964297+Charly4910@users.noreply.github.com>
2025-07-06 16:51:32 -0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Tu configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAzcZgC5TpJEzz2xDpgYMJp3ycJj7izRcA",
   authDomain: "localiza-alistamientos-6a6b9.firebaseapp.com",
   projectId: "localiza-alistamientos-6a6b9",
-  storageBucket: "localiza-alistamientos-6a6b9.firebasestorage.app",
+  storageBucket: "localiza-alistamientos-6a6b9.appspot.com", // 🔧 CORREGIDO
   messagingSenderId: "504938867763",
   appId: "1:504938867763:web:7718feb7f5964583bfc746",
   measurementId: "G-3ZNXPES7S1"
 };
 
-// Initialize Firebase
+// Inicializa la app
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+getAnalytics(app);
+
+// 🔥 EXPORTA Firestore y Storage
+export const db = getFirestore(app);
+export const storage = getStorage(app);
